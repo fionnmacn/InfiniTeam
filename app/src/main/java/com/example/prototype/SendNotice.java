4efa82dae77b74aea9ed7ab1c03487a46e7e3e98
Fionn MacNamara <fionnmacn@gmail.com>
2021-03-30 02:48:12 +0100

    - Sending notices now works fully

--- app/src/main/java/com/example/prototype/SendNotice.java
+++ app/src/main/java/com/example/prototype/SendNotice.java
@@ -1,7 +1,10 @@
 package com.example.prototype;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,14 +13,19 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SendNotice extends AppCompatActivity {
 
-    TextView subject;
-    TextView content;
+    EditText subject;
+    EditText content;
     SwitchCompat priority;
     Button send;
 
@@ -30,6 +38,19 @@ public class SendNotice extends AppCompatActivity {
         priority = findViewById(R.id.send_notice_priority);
         send = findViewById(R.id.send_notice_button);
 
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MESSAGE", String.valueOf(subject.getText()));
+                Log.d("MESSAGE", String.valueOf(content.getText()));
+                Log.d("MESSAGE", String.valueOf(priority.isChecked()));
 
+                ParseObject notice = new ParseObject("Notices");
+                notice.put("subject", String.valueOf(subject.getText()));
+                notice.put("content", String.valueOf(subject.getText()));
+                notice.put("priority", priority.isChecked());
+                notice.saveInBackground();
+            }
+        });
     }
 }
