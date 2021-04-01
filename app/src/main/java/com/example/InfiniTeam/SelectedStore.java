55fe74b7ef1cbacfd3746c80d4a36d3f12612845
Fionn MacNamara <fionnmacn@gmail.com>
2021-04-01 02:13:02 +0100

    - New activity added when clicking on an individual store in the stock list. Allows the user to reserve stock in the store (not plugged into a database so doesn't properly function).

--- app/src/main/java/com/example/InfiniTeam/SelectedStore.java
+++ app/src/main/java/com/example/InfiniTeam/SelectedStore.java
@@ -9,9 +9,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.parse.FindCallback;
 import com.parse.GetDataCallback;
 import com.parse.ParseException;
@@ -25,36 +27,36 @@ public class SelectedStore extends AppCompatActivity {
 
     TextView store;
     TextView stock;
-    Button stock_button;
+    Button reserve_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_selected_product);
+        setContentView(R.layout.activity_selected_store);
 
-        store = findViewById(R.id.product_selected_id);
-        stock = findViewById(R.id.product_selected_stock);
-        stock_button = findViewById(R.id.product_selected_stock_button);
+        store = findViewById(R.id.store_selected_name);
+        stock = findViewById(R.id.store_selected_stock);
+        reserve_button = findViewById(R.id.reserve_button);
 
         Intent intent = getIntent();
         if (intent.getExtras() != null) {
-            ProductModel productModel = (ProductModel) intent.getSerializableExtra("data");
+            StockModel stockModel = (StockModel) intent.getSerializableExtra("data");
 
-            stock.setText("16");
-
-
-//            price.setText((int) productModel.getPrice());
-//            stock.setText(productModel.getStock());
+            String intent_store = stockModel.getStore();
+            String intent_stock = String.valueOf(stockModel.getStock());
+            store.setText(intent_store);
+            stock.setText(intent_stock);
         }
 
-//        stock_button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(SelectedProduct.this, Stock.class)
-//                        .putExtra("id", id.getText());
-//                startActivity(intent);
-//                finish();
-//            }
-//        });
+        reserve_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SelectedStore.this, "Stock successfully reserved",
+                        Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(SelectedStore.this, Products.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
